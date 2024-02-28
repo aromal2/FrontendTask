451a730000b61bcb530d4411bcd5ec2b97f3ae60
Aromal PP <aromalpp783@gmail.com>
2024-02-28 22:10:21 +0530

    commit1

--- src/components/Design.jsx
+++ src/components/Design.jsx
@@ -38,7 +38,15 @@ const toggleDropdown4 = () => {
 
  
  const handleRadioClick = (value) => {
-   setSelectedRadio(value);
+
+   if (selectedRadio === value) {
+  //   // If the clicked option is already selected, deselect it
+     setSelectedRadio(null);
+   } else {
+  //   // Otherwise, select the clicked option
+     setSelectedRadio(value);
+  }
+
 
  };
 
@@ -49,21 +57,26 @@ const toggleDropdown4 = () => {
     <div className="main">
       <div className="line-wrapper">
         <div className="line"></div>
-        <div className="text">Bundle & Save</div>
-        <div className="line"></div>
+        <div className="text">YAY! It's BOGO</div>
+        <div className="line1"></div>
       </div>
 
-      <div className={`second ${selectedRadio ==='option1' ? 'selected-border' : ''}`}>
+      <div
+        className={`second ${
+          selectedRadio === "option1" ? "selected-border" : ""
+        }`}
+      >
         <div className="seconddividing1">
           <div className="secondfirst">
             <div>
               <form className="form">
                 <input
                   type="radio"
-                  id="option2"
+                  id="option1"
                   name="options"
-                  value="option2"
+                  value="option1"
                   className="radio1"
+                  checked={selectedRadio === "option1"}
                   onClick={() => handleRadioClick("option1")}
                 />
                 {/* <label for="option2" className="onepair">
@@ -75,19 +88,33 @@ const toggleDropdown4 = () => {
                 </label>
                 <br></br> */}
                 <div className="pair1">
-                  <p className="pair1pair">1 Pair</p>
-                  <p className="twopair2">DKK 195.00</p>
+                  <p className="pair1pair">
+                    1 Unit <span className="off1">20% OFF</span>
+                  </p>
+                  <p className="twopair2">Standard Price</p>
                 </div>
               </form>
             </div>
           </div>
-          <div className="secondsecond">
-            <p>50% OFF</p>
+          {/* <div>
+            jjjjjjjj
+          </div> */}
+          <div>
+            <p className="secondsecond">$10.00 USD</p>
+            <p className="secondsecond2">$24.00 USD</p>
           </div>
         </div>
       </div>
+      <div className="thirdtop">
+        <div className="thirdtop1"></div>
+        <div className="trapezoid"></div>
+      </div>
 
-      <div className={`third ${selectedRadio ==='option2' ? 'selected-border' : ''}`}>
+      <div
+        className={`third ${
+          selectedRadio === "option2" ? "selected-border" : ""
+        }`}
+      >
         <div className="thirddivi">
           <div className="thirddividing1">
             <div>
@@ -98,28 +125,30 @@ const toggleDropdown4 = () => {
                   name="options"
                   value="option2"
                   className="radio2"
+                  checked={selectedRadio === "option2"}
                   onClick={() => handleRadioClick("option2")}
                 />
               </form>
             </div>
+
             <div className="pair2">
-              <p className="twopair">2 Pair</p>
-              <p className="twopair2">DKK 345.00</p>
+              <p className="twopair">2 Unit</p>
             </div>
           </div>
           <div className="crossed-text">
-            <p>DKK 195.00</p>
+            <p className="rec23">20% Off</p>
           </div>
           <div className="thirdthird">
-            <p className="popular">Most popular</p>
-            <p className="thirdthird2">40% OFF </p>
+            <p className="rectangle2">MOST POPULAR</p>
+            <p className="popular">$18.00 USD</p>
+            <p className="thirdthird2">$24.00 USD </p>
           </div>
         </div>
 
         <div className="table">
           <div className="firstcolumn">
             <p> </p>
-            <p className="data">#1</p>
+            <p className="data1">#1</p>
             <p className="data">#2</p>
           </div>
           <div className="secondcolumn">
@@ -133,19 +162,28 @@ const toggleDropdown4 = () => {
                   <div className="dropdown-options" id="dropdownOptions">
                     <div
                       className="option"
-                      style={{ border: "1px solid #007F61" }}
+                      style={{
+                        border: "1px solid border: #FF6B82",
+                        backgroundColor: "#FF6B82",
+                      }}
                     >
                       S
                     </div>
                     <div
                       className="option"
-                      style={{ border: "1px solid #007F61" }}
+                      style={{
+                        border: "1px solid  #FF6B82",
+                        backgroundColor: "#FF6B82",
+                      }}
                     >
                       M
                     </div>
                     <div
                       className="option"
-                      style={{ border: "1px solid #007F61" }}
+                      style={{
+                        border: "1px solid #FF6B82",
+                        backgroundColor: "#FF6B82",
+                      }}
                     >
                       L
                     </div>
@@ -163,19 +201,28 @@ const toggleDropdown4 = () => {
                   <div className="dropdown-options" id="dropdownOptions">
                     <div
                       className="option"
-                      style={{ border: "1px solid #007F61" }}
+                      style={{
+                        border: "1px solid grey",
+                        backgroundColor: "#FF6B82",
+                      }}
                     >
                       S
                     </div>
                     <div
                       className="option"
-                      style={{ border: "1px solid #007F61" }}
+                      style={{
+                        border: "1px solid grey",
+                        backgroundColor: "#FF6B82",
+                      }}
                     >
                       M
                     </div>
                     <div
                       className="option"
-                      style={{ border: "1px solid #007F61" }}
+                      style={{
+                        border: "1px solid grey;",
+                        backgroundColor: "#FF6B82",
+                      }}
                     >
                       L
                     </div>
@@ -190,25 +237,34 @@ const toggleDropdown4 = () => {
             <div className="dropdown-box">
               <div className="dropdown" onClick={toggleDropdown3}>
                 <div className="dropdown-content">
-                  Colour<span className="arrow-down1">&#9660;</span>
+                  Black<span className="arrow-down2">&#9660;</span>
                 </div>
                 {isOpen3 && (
                   <div className="dropdown-options" id="dropdownOptions">
                     <div
                       className="option"
-                      style={{ border: "1px solid #007F61" }}
+                      style={{
+                        border: "1px solid grey",
+                        backgroundColor: "#FF6B82",
+                      }}
                     >
                       S
                     </div>
                     <div
                       className="option"
-                      style={{ border: "1px solid #007F61" }}
+                      style={{
+                        border: "1px solid grey",
+                        backgroundColor: "#FF6B82",
+                      }}
                     >
                       M
                     </div>
                     <div
                       className="option"
-                      style={{ border: "1px solid #007F61" }}
+                      style={{
+                        border: "1px solid grey",
+                        backgroundColor: "#FF6B82",
+                      }}
                     >
                       L
                     </div>
@@ -225,19 +281,28 @@ const toggleDropdown4 = () => {
                   <div className="dropdown-options" id="dropdownOptions">
                     <div
                       className="option"
-                      style={{ border: "1px solid #007F61" }}
+                      style={{
+                        border: "1px solid grey",
+                        backgroundColor: "#FF6B82",
+                      }}
                     >
                       Black
                     </div>
                     <div
                       className="option"
-                      style={{ border: "1px solid #007F61" }}
+                      style={{
+                        border: "1px solid grey",
+                        backgroundColor: "#FF6B82",
+                      }}
                     >
                       White
                     </div>
                     <div
                       className="option"
-                      style={{ border: "1px solid #007F61" }}
+                      style={{
+                        border: "1px solid grey",
+                        backgroundColor: "#FF6B82",
+                      }}
                     >
                       Grey
                     </div>
@@ -249,10 +314,14 @@ const toggleDropdown4 = () => {
         </div>
       </div>
 
-      <div className={`fourth  ${selectedRadio ==='option3' ? "selected-border" : ""}`}>
+      <div
+        className={`fourth  ${
+          selectedRadio === "option3" ? "selected-border" : ""
+        }`}
+      >
         <div className="seconddividing1">
           <div className="secondfirst">
-            <div>
+            <div className="third4">
               <form className="form">
                 <input
                   type="radio"
@@ -260,6 +329,7 @@ const toggleDropdown4 = () => {
                   name="options"
                   value="option2"
                   className="radio1"
+                  checked={selectedRadio === "option3"}
                   onClick={() => handleRadioClick("option3")}
                 />
                 {/* <label for="option2" className="onepair">
@@ -270,30 +340,36 @@ const toggleDropdown4 = () => {
                   DKK 195.00
                 </label>
                 <br></br> */}
-
-                <div className="pair1">
-                  <p className="pair1pair">3 Pair</p>
-                  <p className="twopair2">DKK 528.00</p>
-                </div>
               </form>
             </div>
+            <div>
+              <p className="pair3">3 Unit</p>
+            </div>
           </div>
-          <div className="secondsecond">
-            <p>50% OFF</p>
+          <div>
+            <div className="off3">30% Off</div>
+          </div>
+          <div className="thirdthirdthird">
+            <p className="thirdthird3">$24.00 USD</p>
+            <p className="second2">$24.00 USD</p>
           </div>
         </div>
       </div>
 
       <div className="fifth">
-        <p className="free">Free 2 Day Shipping</p>
-        <p className="total">Total :DKK 360.00</p>
+        <p className="free">Free Delivery</p>
+        <p className="total">Total :$18.00 USD</p>
       </div>
       <div className="sixth">
-        <button className="button">
-          <span className="cart">&#43; Add to Cart</span>
-        </button>
+        <div className="plus-sign">
+          <div className="horizontal-line"></div>
+          <div className="vertical-line"></div>
+        </div>
+        <div className="cart1">
+          <span className="cart"> Add to Cart</span>
+        </div>
       </div>
-      <p className="power">Powered By Pumper</p>
+      <p className="power">@ Powered By Pumper</p>
     </div>
   );
 };
